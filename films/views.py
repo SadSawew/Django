c59edca8bf92225586199838423887a908846ed3
Vitaly <madzarv27@gmail.com>
2025-01-17 15:01:12 +0300

    Les8 5min

--- films/views.py
+++ films/views.py
@@ -8,4 +8,11 @@ class MovieView(View):
     """Списки фильмов"""
     def get(self, request):
         movies = Movie.objects.all()
-        return render(request, 'movies/movies.html', {"movie_list": movies})
+        return render(request, 'movies/movies.html', {'movie_list': movies})
+    
+
+class MovieDetailView(View):
+    """Описание фильмов"""
+    def get(self, request, slug):
+        movie = Movie.objects.get(url=slug)
+        return render(request, 'movies/movie_detail.html', {'movie': movie})
